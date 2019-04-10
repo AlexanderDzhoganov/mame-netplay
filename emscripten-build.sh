98734e09ae6801fc3ac99defb458617e8dae1730
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2019-04-10 05:20:08 +0300

    update emscripten-build for small filesize

--- emscripten-build.sh
+++ emscripten-build.sh
@@ -3,10 +3,10 @@
 rm -f *.wasm *.js *.bc
 
 emmake make -j5 \
-  SUBTARGET=cps1 SOURCES=src/mame/drivers/cps1.cpp VERBOSE=0 \
-  USE_QTDEBUG=0 DONT_USE_NETWORK=1 NO_USE_MIDI=1 NO_USE_PORTAUDIO=1 \
+  SUBTARGET=cps1 SOURCES=src/mame/drivers/cps1.cpp \
+  VERBOSE=0 USE_QTDEBUG=0 DONT_USE_NETWORK=1 NO_USE_MIDI=1 NO_USE_PORTAUDIO=1 \
   WEBASSEMBLY=1 TESTS=0 BENCHMARKS=0 PROFILER=0 PROFILE=0 \
-  SYMBOLS=1 OPTIMIZE=3 FASTDEBUG=0 DEBUG=0 PTR64=0
+  SYMBOLS=0 OPTIMIZE=s FASTDEBUG=0 DEBUG=0 PTR64=0
 
 cp mamecps1.js ../emushare/frontend/public/
 cp mamecps1.wasm ../emushare/frontend/public/
